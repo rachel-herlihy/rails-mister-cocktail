4fcc026d3952271e0b8ebab9b4beb564c479d3a0
rachel herlihy <rachellouise.herlihy@gmail.com>
2021-01-30 15:33:30 +0000

    m cocktail

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,4 +3,5 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
   validates :description, presence: true
   validates :ingredient, presence: true
+  validates_uniqueness_of :cocktail, scope: :ingredient
 end
