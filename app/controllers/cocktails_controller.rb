98a11ad6e256f97ded81d8053034f8cdf5e51408
rachel herlihy <rachellouise.herlihy@gmail.com>
2021-02-02 21:30:00 +0000

    challenge

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,22 @@
 class CocktailsController < ApplicationController
-  def home
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def shows
+    @cocktails = Cocktail.find(params[:id])
     @cocktails = Cocktail.new
+  end
+  def new
+    @cocktails = Cocktail.new
+  end
+
+  def create
+    @cocktails = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path(@cocktails)
+    else
+      render "new"
+    end
   end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,10 @@
 class DosesController < ApplicationController
+  def create
+    @cocktails = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to cocktails_path(@cocktails)
+    else
+      render "cocktails/show"
+  end
 end
